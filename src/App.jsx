baae3cb333b28f18f87153c6dbe202db1f3fcc0e
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-19 07:02:38 +0300

    solved tailwind configuration error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="bg-yellow-500">
+        <h2 className="text-6xl">testing tailwind</h2>
+      </div>
+      {/* <SimpleInput /> */}
+    </>
+  );
 }
 
 export default App;
