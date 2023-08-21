d3152641b82caf98f9c389ddfc10efdd493a63b0
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-21 04:22:51 +0300

    refactor of the custom form hook

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -3,41 +3,48 @@ import useInput from "./hooks/use-input";
 const SimpleInput = (props) => {
   // in the hook we will happ the function that will be to do validation inside the hook
   // this helps make the hook re-usable for example input
-  const {
-    enteredValue: enteredName,
-    isValid: inputNameIsValid,
-    hasError: nameInputHasError,
-    reset: resetInputName,
-    valueChangeHandler: inputNameHandler,
-    inputOnBlurHandler: inputNameOnBlurHandler,
-  } = useInput((value) => {
-    value.trim() !== "";
-  });
+  const [enteredName, setEnteredName] = useState("");
+  // const [i]
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const inputChangeHandler = (e) => {
+    console.log("i have been called");
+    setEnteredName(e.target.value);
+  };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
-    // setEnteredNameTouched(true);
-    if (!enteredName) {
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
       return;
     }
-    console.log("entered name is", enteredName);
-    resetInputName;
+    setEnteredNameIsValid(true);
+
+    console.log("entered name", enteredName);
+    // setEnteredNameTouched(true);
+    setEnteredName("");
   };
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control ";
 
   return (
-    <div className="py-3 px-5 m-7 bg-gray-600 rounded-xl">
+    <div className="py-3 px-5 m-7 rounded-xl">
       <form onSubmit={formSubmitHandler} className="m-5 ">
-        <div className="form-control">
+        <div className={nameInputClasses}>
           <label htmlFor="name">Your Name</label>
           <input
             className=""
-            onChange={inputNameHandler}
+            onChange={inputChangeHandler}
             type="text"
             id="name"
-            onBlur={inputNameOnBlurHandler}
           />
-          {!nameInputHasError && (
-            <p className="text-red-500">Input can not be empty</p>
+          {nameInputIsInvalid && (
+            <p className="text-red-500 text-sm">Input field cannot be emtpy</p>
           )}
         </div>
         <div className="form-actions">
